df0d6d3a6048ecf904018e8d5a940a4d780b33f3
ANTHONY WOO <anthony.woo14@gmail.com>
2023-01-25 14:04:00 -0700

    successfully retrieved user inputs for Sign Up Form

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+import { signUpUser } from './fetch-utils.js';
+
 const signInForm = document.getElementById('sign-in');
 
 const signUpForm = document.getElementById('sign-up');
 
 // Wire up sign in and sign up forms to supabase
+
+signUpForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(signUpForm);
+    const response = [formData.get('email'), formData.get('password')];
+});
 // Redirect to /other-page on successful auth
 // Redirect to /other-page when page loads if user is authenticated
--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,7 @@
 // Enter Supabase Information
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://xymrqgrvifsouxppmhgn.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5bXJxZ3J2aWZzb3V4cHBtaGduIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQyNTU0ODMsImV4cCI6MTk4OTgzMTQ4M30.4HEq-XtAOWoYJ8ORsZ4vhxk-S8ri8ZYiulgXBYkUSZY';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -8,7 +9,12 @@ export function getUser() {
     return client.auth.session() && client.auth.session().user;
 }
 
-export async function signupUser(email, password) {}
+export async function signUpUser(email, password) {
+    const { data, error } = await supabase.auth.admin.createUser({
+        email: email,
+        password: password,
+    });
+}
 
 export async function signInUser(email, password) {}
 
