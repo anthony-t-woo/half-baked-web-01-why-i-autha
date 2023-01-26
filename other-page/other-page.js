66c63f5448dffed5450897632906a08ca595a8bb
ANTHONY WOO <anthony.woo14@gmail.com>
2023-01-25 17:20:55 -0700

    message

--- other-page/other-page.js
+++ other-page/other-page.js
@@ -7,7 +7,7 @@ window.addEventListener('load', async () => {
     await checkAuth();
     let user = await getUser();
     console.log(user.email);
-    messageEl.textContent = `Hello, you are logged in as ${user.email}`;
+    messageEl.textContent = `Hello, you are logged in as ${user.email}. Have a nice day`;
 });
 
 // add event listener to the logout button and call logout
