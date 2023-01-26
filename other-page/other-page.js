f678413a785026e9432b849d54692b5d3eb20f0c
ANTHONY WOO <anthony.woo14@gmail.com>
2023-01-25 17:08:34 -0700

    changed log in message to be personalized to user

--- other-page/other-page.js
+++ other-page/other-page.js
@@ -1,9 +1,13 @@
-import { checkAuth, logout } from '../fetch-utils.js';
+import { checkAuth, getUser, logout } from '../fetch-utils.js';
 
+const messageEl = document.querySelector('#logged-in-message');
 const logOutButtonEl = document.querySelector('#logout');
 // use checkAuth function to redirect if user not authenticated
 window.addEventListener('load', async () => {
     await checkAuth();
+    let user = await getUser();
+    console.log(user.email);
+    messageEl.textContent = `Hello, you are logged in as ${user.email}`;
 });
 
 // add event listener to the logout button and call logout
